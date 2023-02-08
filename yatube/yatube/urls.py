b9fefd3043a6b4322063d00369e2d115f7a50787
Vladislav <funte@>
2023-02-08 14:17:29 +0300

    Final

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.defaults import page_not_found, permission_denied
 
 urlpatterns = [
     path('auth/', include('users.urls')),
